23a58822992f751bef9d897f4e8365d49770d77d
Jesús Fco Cortés <jfcr@live.com>
2018-06-05 12:06:29 -0500

    INSERTIRNG ANIMATION BASIC

---
+++ src/app/animations.ts
@@ -0,0 +1,159 @@
+import { animate, AnimationEntryMetadata, state, style, transition, trigger } from '@angular/core';
+
+// Component transition animations
+export const ScaleDownUpAnimation: AnimationEntryMetadata =
+  trigger('ScaleDown', [
+    state('1',
+      style({
+        opacity: 0,
+        transform: 'scale(.5)',  
+      })
+    ),
+
+    state('2',
+        style({
+          opacity: 1,
+          transform: 'scale(1)',        
+        })
+      ),
+      
+    transition('1 <=> 2' , animate('300ms ease-out')),
+    
+  ]);
+
+export const backgroundOpacity = 
+    trigger('card', [
+          
+      state('initial', style({
+        transform: 'translate3d(0,50%,0) scale(.8)',                
+      })),
+
+      state('final' ,style({
+        transform: 'translate3d(0,0,0) scale(1)',       
+        
+      })),      
+
+      transition('initial <=> final' , animate('300ms ease-out')),
+    ]);
+
+export const cardPop = 
+  trigger('background', [
+    
+    state('initial', style({        
+      opacity: 0
+    })),
+
+    state('final' ,style({
+            
+      opacity: 1
+    })),      
+
+    transition('initial <=> final' , animate('250ms ease-out')),
+  ]);    
+
+  export const Card: AnimationEntryMetadata = 
+trigger('card', [
+      
+    state('initial', style({
+      transform: 'translate3d(0,50%,0) scale(.7)',                
+    })),
+
+    state('final' ,style({
+      transform: 'translate3d(0,0,0) scale(1)',       
+      
+    })),      
+
+    transition('initial <=> final' , animate('350ms ease-out')),
+  ]);
+
+ export const BackgroundCard = trigger('background', [
+    
+    state('initial', style({        
+      opacity: 0
+    })),
+
+    state('final' ,style({
+            
+      opacity: 1
+    })),      
+
+    transition('initial <=> final' , animate('250ms ease-out')),
+  ]);
+
+  export const LogoPop: AnimationEntryMetadata = 
+
+trigger('loaderImg', [
+      
+    state('initial', style({
+      transform: 'translate3d(0,80%,0) scale(.7)',        
+      opacity: 0
+    })),
+
+    state('final' ,style({
+      transform: 'translate3d(0,0,0) scale(1)',       
+      opacity: 1
+    })),      
+
+    transition('initial <=> final' , animate('1000ms ease-out')),
+  ]);
+
+export const BackgroundLogo =
+  trigger('loader', [
+    
+    state('initial', style({        
+      opacity: 1
+    })),
+
+    state('final' ,style({
+      display: 'none',       
+      opacity: 0
+    })),      
+
+    transition('initial <=> final' , animate('500ms ease-out')),
+  ]);
+
+  export const SlideAnimation: AnimationEntryMetadata = 
+
+trigger('principal', [                
+        state('*' ,style({
+        transform: 'translate3d(0,0,0)',               
+        })),      
+        
+        transition(':enter', [
+        style({
+            transform: 'translate3d(100%,0,0)',
+        }),
+        animate('550ms ease-out')
+        ]),
+
+        transition('void => *', [
+        style({
+            transform: 'translate3d(100%,0,0)',          
+        }),
+        animate('350ms ease-in')
+        ]),
+    ]);
+
+export const FadeAnimation: AnimationEntryMetadata = 
+
+trigger('background', [
+        
+        state('*', style({        
+        opacity: .65
+        })),                  
+        
+        transition(':enter', [
+        style({
+            opacity: 0,          
+        }),
+        animate('0.3s ease-out')
+        ]),
+
+        transition(':leave', [
+        animate('0.5s ease-out', style({
+            opacity: 0,          
+        }))
+        ])
+
+    ])
+;
