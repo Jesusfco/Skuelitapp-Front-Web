b0686f1bc61ce1e42dc3dfdef82e67c94bfa061c
Jesús Fco Cortés <jfcr@live.com>
2018-06-15 10:45:03 -0500

    Parents Can see schedule from children

--- src/app/parents/parents-schedules/parents-schedules.component.ts
+++ src/app/parents/parents-schedules/parents-schedules.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Schedule } from '../../class/Schedule';
+import { ParentsService } from '../parents.service';
+import { User } from '../../class/User';
+
 
 @Component({
   selector: 'app-parents-schedules',
@@ -7,9 +11,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentsSchedulesComponent implements OnInit {
 
-  constructor() { }
+  public schedules: Array<Schedule> = [];
+  public days = [
+    {id: 1, name: 'LUNES'},
+    {id: 2, name: 'MARTES'},
+    {id: 3, name: 'MIERCOLES'},
+    {id: 4, name: 'JUEVES'},
+    {id: 5, name: 'VIERNES'},
+    {id: 6, name: 'SABADO'},
+  ];
+
+  public children: Array<User> = [];
+  public select: any;
+
+  constructor(private _http: ParentsService) {
+    this.setChildren();
+   }
+
 
   ngOnInit() {
+  }
+
+  setChildren() {
+    this._http.getChildrens().then(
+      data => {
+        this.children = []; 
+        for(let d of data) {
+          const x: User = new User();
+          x.setData(d);
+          this.children.push(x);
+        }
+
+        if(this.children.length > 0) {
+          this.select = this.children[0].id;
+          this.setSchedules();
+        }
+
+      },
+      error => sessionStorage.setItem('request', JSON.stringify(error))
+    );
+  }
+
+  setSchedules() {
+    this._http.getSchedule(this.select).then(
+      data => {
+        this.schedules = [];
+        for(let d of data) {
+          const x: Schedule = new Schedule();
+          x.setData(d);
+          this.schedules.push(x);
+        }
+      },
+      error => sessionStorage.setItem('request', JSON.stringify(error))
+    );
   }
 
 }
